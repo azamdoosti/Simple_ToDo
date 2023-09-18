e333b65a97081753bd6e0e675c69558833aef6e9
doosti <a.doosti1@gmail.com>
2023-09-18 14:33:26 +0430

    states and hande input

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import react, { ChangeEvent, useState } from "react"
+import './App.css';
+import { ITask } from "./Interfaces";
+ 
+const App=()=> {
+const [task,setTask]=useState<string>("")
+const [deadline,setDeadline]=useState<number>(0)
+const [todoList,setTodoList]=useState<ITask[]>([])
+
+const handleChange=(event:ChangeEvent<HTMLInputElement>):void=>{
+  if(event.target.name === "task"){
+    setTask(event.target.value)
+  } else {
+    setDeadline(Number(event.target.value))
+  }
+const addTask =() :void=>{
+ setTodoList([...todoList,task])
+}
+
+}
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="inputContainer">
+        <input type="text" placeholder="Task..." name="task" onChange={handleChange}/>
+        <input type="number" placeholder="Deadline(in Days)... name="deadline" />
+        </div>
+        <button>Add Task</button>
+      </div>
+      <div className="todoList"></div>
+    </div>
+  );
+}
+
+export default App;
+
+
+
